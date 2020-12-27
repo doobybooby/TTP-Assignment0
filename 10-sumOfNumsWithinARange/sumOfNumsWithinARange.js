018c23740208af7e727953ae5e8f65947872894c
doobybooby <doobinl0003@gmail.com>
2020-12-27 16:05:39 -0500

    passed sumOfNumsWithinARange.js

--- 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
+++ 10-sumOfNumsWithinARange/sumOfNumsWithinARange.js
@@ -1,5 +1,12 @@
 function sumOfNumsWithinARange(nums, start, end) {
   // Insert code here;
+  var output =0;
+  for(var i =0;i<nums.length;i++){
+    if(nums[i] >= start && nums[i]<=end){
+	    output++;
+    }
+  }
+  return output;
 }
 
 // Do not edit this line;
