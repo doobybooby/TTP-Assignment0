f2504ed09b60ea45032c14c07e9e9f17a2614cec
doobybooby <doobinl0003@gmail.com>
2020-12-27 15:17:16 -0500

    passed countOfAllNumbersSmallerThanTarget.js

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,5 +1,13 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
   // Insert code here;
+  var output =0;
+  var i;
+  for(i=0;i<nums.length;i++){
+    if(nums[i]<target){
+	    output++;
+    }
+  }
+  return output;
 }
 
 // Do not edit this line;
