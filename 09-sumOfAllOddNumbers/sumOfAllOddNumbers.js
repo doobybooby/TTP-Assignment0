e19f332db6d8c9dbad03f5d44a7a45c75c5136ad
doobybooby <doobinl0003@gmail.com>
2020-12-27 15:46:37 -0500

    passed sumOfAllOddNumbers.js

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,5 +1,12 @@
 function sumOfAllOddNumbers(nums) {
   // Insert code here;
+  var output =0;
+  for(var i=0;i<nums.length;i++){
+    if((nums[i]%2)==1){
+	    output++;
+    }
+  }
+  return output;
 }
 
 // Do not edit this line;
