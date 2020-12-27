7cb80f39774bdacd90e6a40a6292603b7a41d38c
doobybooby <doobinl0003@gmail.com>
2020-12-27 15:22:20 -0500

    passed countOfAllIndexMatchingNumbers

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,5 +1,13 @@
 function countOfAllIndexMatchingNumbers(nums) {
   // Insert code here;
+  var output =0;
+  var i;
+  for(i =0;i<nums.length;i++){
+    if(i==nums[i]){
+	    output++;
+    }
+  }
+  return output;
 }
 
 // Do not edit this line;
