ac8d3febed8df22c64bff38131ceab2024947a02
doobybooby <doobinl0003@gmail.com>
2020-12-27 11:41:33 -0500

    passed titleCaseEdit.js

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,14 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  var split = title.split(' ');
+  var output = '';
+  var i;
+  for(i=0;i<split.length;i++){
+	output += split[i].charAt(0).toUpperCase()+split[i].slice(1);
+	if(i!=split.length-1)
+	   output += ' ';
+  }
+  return output;  
 }
 
 // Do not edit this line;
