bcdbaaf9cb7447d486f3a8118aa31afe260eec78
doobybooby <doobinl0003@gmail.com>
2020-12-27 15:01:53 -0500

    passed countOfAllBooleans.js

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,14 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+  var output = 0;
+  var i;
+  
+  for(i=0;i<arr.length;i++){
+    if(arr[i]===true || arr[i]===false){
+	output++;
+    }
+  }
+  return output;
 }
 
 // Do not edit this line;
