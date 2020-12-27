47b3e7241cf941a3726fb138b6ad8d17eed3687f
doobybooby <doobinl0003@gmail.com>
2020-12-27 16:10:47 -0500

    passed sumOfMinimumAndMaximum.js

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,16 @@
 function sumOfMinimumAndMaximum(nums) {
   // Insert code here;
+  var max = nums[0];
+  var min = nums[0];
+  for(var i =0;i<nums.length;i++){
+    if(nums[i]>max){
+	    max= nums[i];
+    }
+    if(nums[i]<min){
+	    min= nums[i];
+    }
+  }
+  return max+min;
 }
 
 // Do not edit this line;
