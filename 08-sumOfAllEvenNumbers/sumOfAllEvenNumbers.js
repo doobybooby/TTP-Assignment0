c1f2084f46ce6ee0dce184d36d854fd5a9353a0f
doobybooby <doobinl0003@gmail.com>
2020-12-27 15:42:22 -0500

    passed sumOfAllEvenNumbers.js

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,5 +1,13 @@
 function sumOfAllEvenNumbers(nums) {
   // Insert code here;
+	var output =0;
+
+for(var i =0;i<nums.length;i++){
+  if((nums[i]%2)==0){
+    output++;
+  }
+}
+ return output;
 }
 
 // Do not edit this line;
