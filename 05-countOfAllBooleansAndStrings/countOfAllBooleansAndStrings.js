3219307cdd75258117abf9e50f93f374c4a028ec
doobybooby <doobinl0003@gmail.com>
2020-12-27 15:08:49 -0500

    passed countOfAllBoolenasAndStrings.js

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,14 @@
 function countOfAllBooleansAndStrings(arr) {
   // Insert code here;
+    var output =0;
+    var i;
+    for(i=0;i<arr.length;i++){
+    if(typeof arr[i]==='boolean' || typeof arr[i]==='string'){
+        output++;
+    }
+  }
+  return output;
+
 }
 
 // Do not edit this line;
