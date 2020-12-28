5943b7959f45a171c6f5082d6a51780b89c640f5
doobybooby <doobinl0003@gmail.com>
2020-12-27 21:35:11 -0500

    passed productOfAnyAmountOfNumbers.js

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,5 +1,10 @@
 function productOfAnyAmountOfNumbers(...args) {
   // Insert code here;
+  var output=1;
+  for(var i=0;i<args.length;i++){
+    output*=args[i];
+  }
+  return output;
 }
 
 // Do not edit this line;
