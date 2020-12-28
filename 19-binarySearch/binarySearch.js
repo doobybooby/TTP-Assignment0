80000abc58514968f3c46d8b5ef6dc59e84fae19
doobybooby <doobinl0003@gmail.com>
2020-12-28 09:42:22 -0500

    fix binarySearch.js

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,18 +5,24 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
-    
-    // Find the middle indez
+    if(nums[0]== target){
+      return true;
+    }
+    // compare the middle index
     var mid = Math.floor((start+end)/2);
     if(nums[mid]== target){
       return true;
     }
+    // if Target is greater than mid, splice, then recurs
     else if(nums[mid]< target && nums.length >1){
       this.binarySearch(nums.splice(mid), target);
     }
+    // if Target is less than mid, splice, then recurs
     else if(nums[mid] > target && nums.length > 1){
       this.binarySearch(nums.splice(0,mid),target);
+
     }
+    // target doesn't exist
     else{
       return false;
     }
