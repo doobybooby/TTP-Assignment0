f4219a37810f4272f692badf9848bcbfaedc4f1d
doobybooby <doobinl0003@gmail.com>
2020-12-27 23:09:38 -0500

    passed pairSum.js, fix 18, fix 19

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,15 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+  var output = new Set();
+  // iterate through each set 
+  for(i in args){
+  // iterate through each element in each set
+    for(j in i){
+	  //add element to output
+	  output.add(i[j]);
+    }
+  }
+  return output;
 }
 
 // Do not edit this line;
--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,7 +5,22 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
-  }
+    
+	  var mid = Math.floor(nums.length/2);
+    if(nums.length<=1){
+	    if(nums[0]!= target){
+	    }
+    }
+    if (target == nums[mid]){
+      return true;
+    }
+    else if(nums[mid]<= target){
+      return this.binarySearch(nums.slice(0,middle),target);
+    }
+    else if(nums[mid]>= target){
+      return this.binarySerach(nums.slice(middle),target);
+    }
+   }
 }
 
 // Do not edit this line;
