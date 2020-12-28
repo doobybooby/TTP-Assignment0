ef954178ae0a31db519d282bdafeb3458eb9ccac
doobybooby <doobinl0003@gmail.com>
2020-12-27 21:19:16 -0500

    passed frequencyCounter.js

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,17 @@
 function frequencyCounter(word) {
   // Insert code here;
+  var dict = {};
+  var i;
+  for(var i=0;i<word.length;i++){
+    var letter = word.charAt(i);
+	  if(dict[letter]){
+	    dict[letter]++;
+	  }
+	  else{
+	    dict[letter]=1;
+	  }
+  }
+  return dict;
 }
 
 // Do not edit this line;
