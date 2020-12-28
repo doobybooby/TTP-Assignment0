9095fe6b0b9c95dec2e07bbe3844118d1fd0d62c
doobybooby <doobinl0003@gmail.com>
2020-12-27 21:27:24 -0500

    passed countDownSum.js

--- 15-countDownSum/countDownSum.js
+++ 15-countDownSum/countDownSum.js
@@ -1,9 +1,15 @@
 class MySolution {
   countDownSum(num) {
     // Insert code here;
+    var output =0;
+    if(num == 1){
+      return 1;
+    }
+    else{
+      return num + this.countDownSum(num-1);
+    }
   }
 }
-
 // Do not edit this line;
 let StudentSolution = MySolution;
 module.exports = StudentSolution;
